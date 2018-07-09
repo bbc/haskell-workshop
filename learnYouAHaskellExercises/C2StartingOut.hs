be36a38d96d094ef18ae40914b1ddc73cbc4c384
chris-bacon <christopher.bacon@bbc.co.uk>
2018-07-09 17:12:14 +0100

    Started chapter 2 exercises

---
+++ learnYouAHaskellExercises/C2StartingOut.hs
@@ -0,0 +1,42 @@
+module C2StartingOut where
+
+-- To load in ghci: :load C2StartingOut.hs
+-- Then you will be able to run all of these functions
+
+-- Write a function to subtract two numbers
+-- Example: sub 100 72 returns 28
+sub = error "TODO sub"
+
+-- Write a function to divide a number by 10
+-- Example: divideByTen 50 returns 5
+divideByTen = error "TODO divideByTen"
+
+-- Write a function that calculates the area of a circle
+-- Hint: Use pi from Prelude
+-- Example: area 10 returns 314.1592653589793 
+area = error "TODO area"
+
+-- Write a function that takes two arguments and concatenates them together
+-- Example: myConcat "hello" "Haskell" returns "hello Haskell"
+myConcat = error "TODO myConcat"
+
+-- Get the third element from a list using only head and tail
+-- Example: myThirdFirst [1,2,3,4,5] returns 3
+myThirdFirst = error "TODO myThirdFirst"
+
+-- Get the third to last element from a list using only last and init
+-- Example myThirdLast [6,7,8,9,10] returns 8
+myThirdLast = error "TODO myThirdLast"
+
+-- Write a function that takes the last three items from a list and sums them
+-- Hint: Use reverse, take, and sum
+-- Example: lastThreeSummed [10,20,30,40,50] returns 120
+lastThreeSummed = error "TODO lastThreeSummed"
+
+-- Write a function that takes the first 8 characters from the english alphabet in a list
+-- Example: upToEight returns ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+upToEight = error "TODO upToEight"
+
+-- Write a list comprehension that calculates fizzBuzz
+-- Example: fizzBuzz returns ["Fizz", "Fizz", "Buzz", ... etc]
+fizzBuzz = error "TODO fizzBuzz"
