f371d489273d30da6eaa07010ba22c8e7d6b738c
chris-bacon <christopher.bacon@bbc.co.uk>
2018-07-19 17:45:55 +0100

    Added test for area

--- learnYouAHaskellExercises/test/C2Tests.hs
+++ learnYouAHaskellExercises/test/C2Tests.hs
@@ -5,9 +5,11 @@ import Test.Hspec
 import qualified C2StartingOut as C2
 
 testC2 = hspec $ do
-    describe "sub" $ do
-        it "is implemented correctly" $ do
+    describe "C2" $ do
+        it "sub is implemented correctly" $ do
             C2.sub 100 72 `shouldBe` 28
             C2.sub 10 0 `shouldBe` 10
             C2.sub 3 2 `shouldBe` 1
+        it "area is implemented correctly" $ do
+            C2.area 10 `shouldBe` 314.1592653589793
 
