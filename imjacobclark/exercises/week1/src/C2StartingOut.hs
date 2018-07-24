a348fb2e08f8c8ce85ebe6cd1bd12e3bc3c9d42a
Jacob Clark <jacob.clark@bbc.co.uk>
2018-07-24 23:01:47 +0100

    Adding missing tests + checking in my solutions to week 1

---
+++ imjacobclark/exercises/week1/src/C2StartingOut.hs
@@ -0,0 +1,46 @@
+module C2StartingOut where
+
+-- To load in ghci: :load C2StartingOut.hs
+-- Then you will be able to run all of these functions
+
+-- Write a function to subtract two numbers
+-- Example: sub 100 72 returns 28
+sub x y = x - y
+
+-- Write a function that calculates the area of a circle
+-- Hint: Use pi from Prelude
+-- Example: area 10 returns 314.1592653589793 
+area r = pi * r^2
+
+-- Take two arguments and concatenate them together
+-- Example: myConcat "hello" " Haskell" returns "hello Haskell"
+myConcat x y = x ++ y
+
+-- Get the third element from a list using only head and tail
+-- Example: myThirdFirst [1,2,3,4,5] returns 3
+getThirdElement xs = head (tail (tail xs))
+
+-- Get the third to last element from a list using only last and init
+-- Example myThirdLast [6,7,8,9,10] returns 8
+getThirdElementLast xs = last (init (init xs))
+
+-- Take the last three items from a list and sums them
+-- Example: lastThreeSummed [10,20,30,40,50] returns 120
+lastThreeSummed xs = sum (take 3 (reverse xs))
+
+-- Get the first n characters from the english alphabet in a list
+-- Example: getLetters 8 returns ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+getLetters x = take x ['a'..'z']
+
+-- Generate a palindrome from any input string (doesn't have to be meaningful)
+-- Example: palindrome "cats and dogs" returns "cats and dogssgod dna stac"
+palindrome x = x ++ (reverse x)
+
+-- Cut the elements of a list between two points
+-- Hint: this function will take 3 arguments (two indices and a list)
+-- Example: cut 10 20 [1..100] returns [11,12,13,14,15,16,17,18,19,20]
+cut min max xs = [ x  | x <- xs, x <= max, x >= min ]
+
+-- Write a list comprehension that returns fizzBuzz
+-- Example: fizzBuzz returns ["Fizz", "Fizz", "Buzz", ... etc]
+fizzBuzz = [ if s `mod` 3 == 0 then "Fizz" else if s `mod` 5 == 0 then "Buzz" else show s | s <- [0..100]]
--- imjacobclark/exercises/week1/test/C2Tests.hs
+++ imjacobclark/exercises/week1/test/C2Tests.hs
@@ -36,4 +36,3 @@ testC2 = hspec $ do
             C2.cut 4 5 [0,1,2,3,4,5] `shouldBe` [4,5]
         it "fizzBuzz is implemented correctly" $ do
             C2.fizzBuzz `shouldBe`["Fizz","1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","Fizz","16","17","Fizz","19","Buzz","Fizz","22","23","Fizz","Buzz","26","Fizz","28","29","Fizz","31","32","Fizz","34","Buzz","Fizz","37","38","Fizz","Buzz","41","Fizz","43","44","Fizz","46","47","Fizz","49","Buzz","Fizz","52","53","Fizz","Buzz","56","Fizz","58","59","Fizz","61","62","Fizz","64","Buzz","Fizz","67","68","Fizz","Buzz","71","Fizz","73","74","Fizz","76","77","Fizz","79","Buzz","Fizz","82","83","Fizz","Buzz","86","Fizz","88","89","Fizz","91","92","Fizz","94","Buzz","Fizz","97","98","Fizz","Buzz"]
-
