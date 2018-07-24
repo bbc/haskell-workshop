9fd19af6c6943edae79ed7f8351f1f89982d8683
chris-bacon <chrisbacon2009@hotmail.com>
2018-07-24 23:17:28 +0100

    Added more exercises

--- learnYouAHaskellExercises/src/C3TypesAndTypeclasses.hs
+++ learnYouAHaskellExercises/src/C3TypesAndTypeclasses.hs
@@ -1,6 +1,5 @@
 module C3TypesAndTypeclasses where
 
--- Functions from the previous chapter
 -- last, length, and take are all from the standard library, Prelude
 -- You can find out the type in ghci with ":t last", but attempt to answer first
 
@@ -13,17 +12,23 @@ module C3TypesAndTypeclasses where
 -- What is the type of the take function?
 -- take :: ??
 
-
--- What is the type of the following functions
--- can you use type classes to constrain the polymorphic types?
-
 -- myConcat :: ??
 myConcat a b = a ++ b
 
--- TODO: This might confuse people since lists already are an instance of Show
--- Hint: This is a weird one because you aren't returning a list but a String because show :: a -> String
--- Can you use Show?
---myConcatAndShow :: ??
-myConcatAndShow a b = show (a ++ b)
 
+-- Can you use type classes to constrain the polymorphic types?
+-- Make the types polymorphic and use a typeclass constraint
+-- What happens if you don't use a typeclass constraint?
+
+-- What is the type of this function
+-- mul :: ??
+mul a b = a * b
+
+-- Consider this function
+hasShowableInside :: Show a => [a] -> Bool
+hasShowableInside xs = null xs
+
+-- Now convert it into a function that checks only whether there are numbers inside
+-- hasNumbersInside :: ??
+hasNumbersInside xs = null xs
 
