c44cf7a60c3d2bc8ef32d11695ee55ae2b09f5be
chris-bacon <chrisbacon2009@hotmail.com>
2018-07-24 20:31:46 +0100

    Began exercises

---
+++ learnYouAHaskellExercises/src/C3TypesAndTypeclasses.hs
@@ -0,0 +1,12 @@
+module C3TypesAndTypeclasses where
+
+-- What is the type of the last function?
+-- last :: ??
+
+-- What is the type of the length function?
+-- length :: ??
+
+-- What is the type of the take function?
+-- take :: ??
+
+
