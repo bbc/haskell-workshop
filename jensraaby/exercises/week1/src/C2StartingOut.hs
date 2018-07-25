f69c1295f47e8b2bf1a40f6c4b053a417a66f126
Jens Raaby <jensraaby@users.noreply.github.com>
2018-07-25 22:38:01 +0100

    Make the C2 tests pass

---
+++ jensraaby/exercises/week1/src/C2StartingOut.hs
@@ -0,0 +1,54 @@
+module C2StartingOut where
+
+-- To load in ghci: :load C2StartingOut.hs
+-- Then you will be able to run all of these functions
+
+-- Write a function to subtract two numbers
+-- Example: sub 100 72 returns 28
+sub a b = a - b 
+
+-- Write a function that calculates the area of a circle
+-- Hint: Use pi from Prelude
+-- Example: area 10 returns 314.1592653589793 
+area r = pi * r * r 
+
+-- Take two arguments and concatenate them together
+-- Example: myConcat "hello" " Haskell" returns "hello Haskell"
+myConcat a b = a ++ b
+
+-- Get the third element from a list using only head and tail
+-- Example: myThirdFirst [1,2,3,4,5] returns 3
+getThirdElement (_:_:z:_) = z  
+-- OK, without cheating:
+getThirdElement2 xs = head (tail (tail xs))
+
+-- Get the third to last element from a list using only last and init
+-- Example myThirdLast [6,7,8,9,10] returns 8
+getThirdElementLast = last . init . init
+
+-- Take the last three items from a list and sums them
+-- Example: lastThreeSummed [10,20,30,40,50] returns 120
+lastThreeSummed xs = sum $ take 3 (reverse xs)
+
+-- Get the first n characters from the english alphabet in a list
+-- Example: getLetters 8 returns ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+getLetters n =  take n ['a'..'z']
+-- not sure whether this counts as a string is a list of chars...
+
+-- Generate a palindrome from any input string (doesn't have to be meaningful)
+-- Example: palindrome "cats and dogs" returns "cats and dogssgod dna stac"
+palindrome xs = xs ++ reverse xs
+
+-- Cut the elements of a list between two points
+-- Hint: this function will take 3 arguments (two indices and a list)
+-- Example: cut 10 20 [1..100] returns [11,12,13,14,15,16,17,18,19,20]
+cut low high xs = take (high-low + 1) (drop low xs)
+
+-- Write a list comprehension that computes Fizz Buzz
+-- Fizz is any number  that is divisible by 3 and Buzz is any number that is divisible by 5
+-- Example: fizzBuzz returns ["Fizz","1","2","Fizz","4","Buzz"... etc
+fizzBuzz = [if x `mod` 3 == 0 then "Fizz" else if x `mod` 5 == 0 then "Buzz" else show x | x <- [0..100]]
+-- I think the above isn't the normal definition of Fizz buzz, but it passes
+-- the test :)
+
+
