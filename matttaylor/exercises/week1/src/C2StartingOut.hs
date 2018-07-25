6de99cb30ba54df7f10c6a6e7abb7b6325426114
matt-t-bbc <matthew.taylor01@bbc.co.uk>
2018-07-26 00:14:04 +0100

    Completed most of exercises

--- matttaylor/exercises/week1/src/C2StartingOut.hs
+++ matttaylor/exercises/week1/src/C2StartingOut.hs
@@ -1,48 +1,53 @@
 module C2StartingOut where
 
+import Data.List
+
 -- To load in ghci: :load C2StartingOut.hs
 -- Then you will be able to run all of these functions
 
 -- Write a function to subtract two numbers
 -- Example: sub 100 72 returns 28
-sub = error "TODO sub"
+sub x y = x - y 
 
 -- Write a function that calculates the area of a circle
 -- Hint: Use pi from Prelude
 -- Example: area 10 returns 314.1592653589793 
-area = error "TODO area"
+area x = pi * x^2
 
 -- Take two arguments and concatenate them together
 -- Example: myConcat "hello" " Haskell" returns "hello Haskell"
-myConcat = error "TODO myConcat"
+myConcat x y = x ++ y 
 
--- Get the third element from a list using only head and tail
+-- Get the third:w
+-- element from a list using only head and tail
 -- Example: myThirdFirst [1,2,3,4,5] returns 3
-getThirdElement = error "TODO getThirdElement"
+getThirdElement l = head (tail (tail l))
 
 -- Get the third to last element from a list using only last and init
 -- Example myThirdLast [6,7,8,9,10] returns 8
-getThirdElementLast = error "TODO getThirdElementLast"
+getThirdElementLast l = last (init (init l))
 
 -- Take the last three items from a list and sums them
 -- Example: lastThreeSummed [10,20,30,40,50] returns 120
-lastThreeSummed = error "TODO lastThreeSummed"
+lastThreeSummed l = sum (take 3 (reverse l))
+
+lastThreeSummed2 l = sum (drop (length l - 3) l)
 
 -- Get the first n characters from the english alphabet in a list
 -- Example: getLetters 8 returns ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
-getLetters = error "TODO getLetters"
+getLetters n = take n ['a','b'..]
 
 -- Generate a palindrome from any input string (doesn't have to be meaningful)
 -- Example: palindrome "cats and dogs" returns "cats and dogssgod dna stac"
-palindrome = error "TODO palindrome"
+palindrome x = x ++ reverse x
 
 -- Cut the elements of a list between two points
 -- Hint: this function will take 3 arguments (two indices and a list)
 -- Example: cut 10 20 [1..100] returns [11,12,13,14,15,16,17,18,19,20]
-cut = error "TODO cut"
+--cut lower upper l = [ x  | x <- l, fst (indexed l) >= lower, fst(indexed l) <= upper]
 
 -- Write a list comprehension that computes Fizz Buzz
 -- Fizz is any number  that is divisible by 3 and Buzz is any number that is divisible by 5
 -- Example: fizzBuzz returns ["Fizz","1","2","Fizz","4","Buzz"... etc
 fizzBuzz = error "TODO fizzBuzz"
-
+i
