5b16ad3a799aa80493c86806a87a03223f57ac65
matt-t-bbc <matthew.taylor01@bbc.co.uk>
2018-08-02 11:54:37 +0100

    Some of C3 exercises done

--- matttaylor/exercises/week1/src/C3TypesAndTypeclasses.hs
+++ matttaylor/exercises/week1/src/C3TypesAndTypeclasses.hs
@@ -4,33 +4,34 @@ module C3TypesAndTypeclasses where
 -- You can find out the type in ghci with ":t last", but attempt to answer first
 
 -- What is the type of the last function?
--- last :: ??
+--last' :: [a] -> a
 
 -- What is the type of the length function?
--- length :: ??
+--length' :: [a] -> Int
+--http://hackage.haskell.org/package/base-4.11.1.0/docs/Prelude.html#v:length
+-- ??
 
 -- What is the type of the take function?
--- take :: ??
+--take :: Int -> [a] -> [a]
 
--- myConcat :: ??
+myConcat :: [a] -> [a] -> [a]
 myConcat a b = a ++ b
-
 
 -- Can you use type classes to constrain the polymorphic types?
 -- Make the types polymorphic and use a typeclass constraint
 -- What happens if you don't use a typeclass constraint?
 
 -- What is the type of this function
--- mul :: ??
+mul :: Num a => a -> a -> a
 mul a b = a * b
 
 -- Consider this function
 hasShowableInside :: Show a => [a] -> Bool
-hasShowableInside xs = null xs
+hasShowableInside _ = True
 
 -- Now convert it into a function that checks only whether there are numbers inside
--- hasNumbersInside :: ??
-hasNumbersInside xs = null xs
+hasNumbersInside :: Num a => [a] -> Bool
+hasNumbersInside [_] = False
 
 -- Write the type signature for this function
 -- Hint: You will need to use the relevant typeclasses
