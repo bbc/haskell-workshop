53f39dbd1b2fc35683ebc0663ec8c367d311597f
Jens Raaby <jensraaby@users.noreply.github.com>
2018-08-05 16:06:36 +0100

    C3 exercises

---
+++ jensraaby/exercises/week2/src/C3TypesAndTypeclasses.hs
@@ -0,0 +1,38 @@
+module C3TypesAndTypeclasses where
+
+-- last, length, and take are all from the standard library, Prelude
+-- You can find out the type in ghci with ":t last", but attempt to answer first
+
+-- What is the type of the last function?
+-- last :: [a] -> a
+
+-- What is the type of the length function?
+-- length :: Foldable t => t a -> Int
+
+-- What is the type of the take function?
+-- take :: Int-> [a] -> [a]
+
+-- myConcat :: [a] -> [a] -> [a]
+myConcat a b = a ++ b
+
+
+-- Can you use type classes to constrain the polymorphic types?
+-- Make the types polymorphic and use a typeclass constraint
+-- What happens if you don't use a typeclass constraint?
+
+-- What is the type of this function
+-- mul :: Num a => a -> a -> a
+mul a b = a * b
+
+-- Consider this function
+hasShowableInside :: Show a => [a] -> Bool
+hasShowableInside xs = null xs
+
+-- Now convert it into a function that checks only whether there are numbers inside
+hasNumbersInside :: Num a => [a] -> Bool
+hasNumbersInside xs = not (null xs)
+
+-- Write the type signature for this function
+-- Hint: You will need to use the relevant typeclasses
+showAndRead :: (Read a, Show a) => a -> a
+showAndRead a = read (show a)
