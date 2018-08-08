1e54b6c8e6b9e7fa6446359567478656ecb06021
chris-bacon <chrisbacon2009@hotmail.com>
2018-08-08 11:59:42 +0100

    Added tests for chapter 4

---
+++ learnYouAHaskellExercises/src/C4SyntaxInFunctions.hs
@@ -0,0 +1,22 @@
+module C4SyntaxInFunctions where
+
+-- Pattern Matching
+
+-- finish this fibonaacci function using pattern matching
+-- fib 10 should return 89
+fib :: Int -> Int
+fib 0 = undefined
+fib 1 = undefined
+fib n = undefined
+
+-- write a safeTail function that returns the tail of a list if it is not empty
+-- otherwise returns an empty list, []
+-- use pattern matching 
+-- (note that the patterns are not filled in like they are in the fib example, you will need to write the patterns yourself)
+safeTail :: [String] -> [String]
+safeTail = undefined
+
+-- write the reverse function using pattern matching
+myReverse :: [a] -> [a]
+myReverse = undefined
+
---
+++ learnYouAHaskellExercises/test/C4Tests.hs
@@ -0,0 +1,17 @@
+module C4Tests where
+
+import Test.Hspec
+
+import qualified C4SyntaxInFunctions as C4
+
+testC4 = hspec $ do
+    describe "C4" $ do
+        it "fib is implemented correctly" $ do
+            C4.fib 10 `shouldBe` 89
+        it "safeHead is implemented correctly" $ do
+            C4.safeTail [] `shouldBe` []
+            C4.safeTail ["a", "b", "c"] `shouldBe` ["b", "c"]
+        it "myReverse is implemented correctly" $ do
+            C4.myReverse ["Manchester", "Leeds", "Bradford"] `shouldBe` ["Bradford", "Leeds", "Manchester"]
+            C4.myReverse [1, 2, 3, 4, 5] `shouldBe` [5, 4, 3, 2, 1]
+
