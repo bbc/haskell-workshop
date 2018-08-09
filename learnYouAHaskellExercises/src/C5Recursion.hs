3f7b0b4b3be162080b9799d7407752a294be88af
chris-bacon <chrisbacon2009@hotmail.com>
2018-08-09 11:50:41 +0100

    Added some exercises for chapter 5

---
+++ learnYouAHaskellExercises/src/C5Recursion.hs
@@ -0,0 +1,17 @@
+module C5Recursion where
+
+-- Write a minimum function that takes a list and returns its smallest element
+myMin :: Ord a => [a] -> a
+myMin = undefined
+
+-- Write a function that takes a finite list and turns it into an infinite list which repeats itself
+-- Hint: this won't need any base/edge case
+longList :: [a] -> [a]
+longList = undefined
+
+-- Write the map function, 
+--  i.e. JavaScript's array.map(function)
+-- using recursion
+myMap :: (a -> b) -> [a] -> [b]
+myMap = undefined
+
