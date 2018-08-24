e007aa92c2b390eb2daa2036f4b27032a0101f13
chris-bacon <christopher.bacon@bbc.co.uk>
2018-08-24 15:00:19 +0100

    Fixed test: thanks Chris Vaughan!

--- learnYouAHaskellExercises/test/C5Tests.hs
+++ learnYouAHaskellExercises/test/C5Tests.hs
@@ -10,11 +10,13 @@ testC5 = hspec $ do
             C5.myMin [4,5,6] `shouldBe` 4
             C5.myMin [10] `shouldBe` 10
             C5.myMin [100, 23, 11] `shouldBe` 11
+        it "longList is implemented correctly" $ do
+            take 9 (C5.longList [1,2,3]) `shouldBe` [1,2,3,1,2,3,1,2,3]
         it "myMap is implememted correctly" $ do
             C5.myMap (+3) [1,1,2] `shouldBe` [4,4,5]
             C5.myMap (*10) [] `shouldBe` []
             C5.myMap (++ " rocks") ["haskell"] `shouldBe` ["haskell rocks"]
         it "tuple2List is implememted correctly" $ do
             C5.tuple2List [(1,2),(1,2)] `shouldBe` [1,1]
-            C5.tuple2List [] `shouldBe` []
+            C5.tuple2List ([] :: [(Int, Int)]) `shouldBe` []
             C5.tuple2List [("a", 1), ("b", 2)] `shouldBe` ["a", "b"]
