b08321ffd7a12ba558c22ef3612070acf256d583
Violeta Menendez Gonzalez <violeta.menendezgonzalez@bbc.co.uk>
2018-09-07 16:18:59 +0100

    Violeta: Add more recursion exercise solutions

--- violeta/exercises/src/C5Recursion.hs
+++ violeta/exercises/src/C5Recursion.hs
@@ -27,3 +27,30 @@ myMap op (x:xs) = (op x) : myMap op xs
 tuple2List :: [(a, b)] -> [a]
 tuple2List [] = []
 tuple2List (x:xs) = (fst x) : tuple2List xs
+
+-- Write the filter function,
+--  i.e. JavaScript's array.filter(fn)
+-- using recursion
+-- myFilter (>2) [1,2,3,4] returns [3, 4]
+myFilter :: (a -> Bool) -> [a] -> [a]
+myFilter _ [] = []
+myFilter fn (x:xs)
+    | fn x = x:myFilter fn xs
+    | otherwise = myFilter fn xs
+
+-- Write the reduce function,
+--  i.e. JavaScript's array.reduce(fn)
+-- using recursion
+-- myReduce (+) 0 [1..100] returns 5050
+myReduce :: (a -> b -> b) -> b -> [a] -> b
+myReduce fn y [x] = fn x y
+myReduce fn y (x:xs) = fn x (myReduce fn y xs)
+
+
+-- Write the map function again, but this time implement it with myReduce
+-- myMapWithReduce (+2) [1,2,3] returns [3,4,5]
+myMapWithReduce :: (a -> b) -> [a] -> [b]
+--myMapWithReduce fn (x:xs) = myReduce (:) [] (in x (myMapWithReduce fn xs))
+myMapWithReduce fn [] = []
+myMapWithReduce fn [x] = [fn x]
+myMapWithReduce fn (x:xs) = myReduce (:) (myMapWithReduce fn xs) (myMapWithReduce fn [x])
--- violeta/exercises/test/C5Tests.hs
+++ violeta/exercises/test/C5Tests.hs
@@ -10,11 +10,24 @@ testC5 = hspec $ do
             C5.myMin [4,5,6] `shouldBe` 4
             C5.myMin [10] `shouldBe` 10
             C5.myMin [100, 23, 11] `shouldBe` 11
+        it "longList is implemented correctly" $ do
+            take 9 (C5.longList [1,2,3]) `shouldBe` [1,2,3,1,2,3,1,2,3]
+        it "tuple2List is implememted correctly" $ do
+            C5.tuple2List [(1,2),(1,2)] `shouldBe` [1,1]
+            C5.tuple2List ([] :: [(Int, Int)]) `shouldBe` []
+            C5.tuple2List [("a", 1), ("b", 2)] `shouldBe` ["a", "b"]
         it "myMap is implememted correctly" $ do
             C5.myMap (+3) [1,1,2] `shouldBe` [4,4,5]
             C5.myMap (*10) [] `shouldBe` []
             C5.myMap (++ " rocks") ["haskell"] `shouldBe` ["haskell rocks"]
-        it "tuple2List is implememted correctly" $ do
-            C5.tuple2List [(1,2),(1,2)] `shouldBe` [1,1]
---            C5.tuple2List [] `shouldBe` []
-            C5.tuple2List [("a", 1), ("b", 2)] `shouldBe` ["a", "b"]
+        it "myFilter is implememted correctly" $ do
+            C5.myFilter (>3) [1,2,3,4,5] `shouldBe` [4,5]
+            C5.myFilter (>10) [] `shouldBe` []
+            C5.myFilter (== "haskell") ["haskell", "javascript"] `shouldBe` ["haskell"]
+        it "myReduce is implememted correctly" $ do
+            C5.myReduce (+) 0 [1,2,3] `shouldBe` 6
+            C5.myReduce (:) [] ["hello", "world"] `shouldBe` ["hello", "world"]
+        it "myMapWithReduce is implememted correctly" $ do
+            C5.myMapWithReduce (+3) [1,1,2] `shouldBe` [4,4,5]
+            C5.myMapWithReduce (*10) [] `shouldBe` []
+            C5.myMapWithReduce (++ " rocks") ["haskell"] `shouldBe` ["haskell rocks"]
