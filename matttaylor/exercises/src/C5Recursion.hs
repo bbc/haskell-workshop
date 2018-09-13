562b8943db846e00115dab64e7a9a63a115c87f4
matt-t-bbc <matthew.taylor01@bbc.co.uk>
2018-09-13 11:39:54 +0100

    Completed exercises

--- matttaylor/exercises/src/C5Recursion.hs
+++ matttaylor/exercises/src/C5Recursion.hs
@@ -3,22 +3,31 @@ module C5Recursion where
 -- Write a minimum function that takes a list and returns its smallest element
 -- myMin [10, 5, 15] returns 5
 myMin :: Ord a => [a] -> a
-myMin = undefined
+myMin [] = error "min of empty list"
+myMin [x] = x
+myMin (x:xs)
+    | x < minVal = x
+    | otherwise = minVal
+    where minVal = myMin xs
+
 
 -- Write a function that takes a finite list and turns it into an infinite list which repeats itself
 -- Hint: this won't need any base/edge case
 -- longList [1,2,3,4,5] returns [1,2,3,4,5,...]
 longList :: [a] -> [a]
-longList = undefined
+longList x = x ++ longList x
+
 
 -- Write the map function, 
 --  i.e. JavaScript's array.map(function)
 -- using recursion
 -- myMap (+2) [1,2,3] returns [3,4,5]
 myMap :: (a -> b) -> [a] -> [b]
-myMap = undefined
+myMap _ [] = []
+myMap f (x:xs) = f x : myMap f xs
 
 -- Write a function that takes a list of tuples and extracts the first element in each tuple into a new list
 -- tuple2List [("a", 1), ("b", 2)] returns ["a", "b"]
 tuple2List :: [(a, b)] -> [a]
-tuple2List = undefined
+tuple2List [] = []
+tuple2List ((x,y):xs) = x : tuple2List xs
