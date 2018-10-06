e23952fad1bad04e0242da455a93638b9099d2c7
chris-bacon <chrisbacon2009@hotmail.com>
2018-10-06 14:26:17 +0100

    Started C11 exercises

---
+++ learnYouAHaskellExercises/src/C11FunctorsEtAl.hs
@@ -0,0 +1,13 @@
+module C11FunctorsEtAl where
+
+data One a = One a
+
+data Two a b = Two a b
+
+data Three a b c = Three a b c
+
+-- Write functor instances for the above datatypes
+
+-- instance Functor One ...
+--     ...
+
